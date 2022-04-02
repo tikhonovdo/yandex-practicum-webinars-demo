3f1a80e60b1bbbebf8267bdcd5d50aa723421442
Tikhonov Dmitry <chitzkoy@gmail.com>
2022-04-02 15:25:24 +0300

    first commit

---
+++ src/StepTracker.java
@@ -0,0 +1,20 @@
+public class StepTracker {
+
+    //массив чисел шагов по датам
+    MonthData[] monthData;
+
+    //цель на день
+    int dailyGoal;
+
+    int averageDailySteps() {
+        return 0;
+    }
+
+
+    class MonthData {
+
+        int[] days = new int[30];
+
+    }
+
+}
