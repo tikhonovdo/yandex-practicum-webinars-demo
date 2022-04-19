780336e1d25a693aa138f98d732b6529a093ba97
Tikhonov Dmitry <chitzkoy@gmail.com>
2022-04-19 20:22:27 +0300

    something

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+//        ArrayList<String> strings = new ArrayList<>();
+//        strings.add("hello");
+//        strings.add(new Integer(42));
+//        strings.add(Boolean.TRUE);
+        String fileContents = "month,amount,is_expense\n" +
+                    "01,100000,false\n" +
+                    "01,30000,true\n" +
+                    "02,321690,false\n" +
+                    "02,130000,true\n" +
+                    "03,999999,true\n" +
+                    "03,999999,false";
+
 
     }
 
